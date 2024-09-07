e26c20381c22d83b30e2b9898849635e67d300eb
199ocero <199ocero@gmail.com>
2024-09-07 18:14:40 +0800

    added geocoding

--- config/openweather.php
+++ config/openweather.php
@@ -11,17 +11,28 @@ return [
     |
     */
 
-    'key' => env('OPENWEATHER_API_KEY'),
+    'api_key' => env('OPENWEATHER_API_KEY'),
 
     /*
     |--------------------------------------------------------------------------
-    | OpenWeather API URL
+    | OpenWeather Weather API URL
     |--------------------------------------------------------------------------
     |
-    | The URL to the OpenWeather API.
+    | The URL to the OpenWeather Weather API.
     |
     */
 
-    'url' => env('OPENWEATHER_API_URL', 'https://api.openweathermap.org/data/3.0'),
+    'weather_api_url' => env('OPENWEATHER_WEATHER_API_URL', 'https://api.openweathermap.org/data/3.0'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | OpenWeather Geocoding API URL
+    |--------------------------------------------------------------------------
+    |
+    | The URL to the OpenWeather Geocoding API.
+    |
+    */
+
+    'geocoding_api_url' => env('OPENWEATHER_GEOCODING_API_URL', 'https://api.openweathermap.org/geo/1.0'),
 
 ];
