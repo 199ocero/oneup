9726d2c213fdd81848a4f3f8bd94a64a41287cc0
199ocero <199ocero@gmail.com>
2024-09-07 19:51:35 +0800

    added attribute to fieldset

--- resources/views/components/fieldset.blade.php
+++ resources/views/components/fieldset.blade.php
@@ -1,4 +1,4 @@
-<div class="relative p-5 rounded-lg border border-gray-300 dark:border-gray-700">
+<div {{ $attributes->merge(['class' => 'relative p-5 rounded-lg border border-gray-300 dark:border-gray-700']) }}>
     <span
         class="absolute -top-3 left-5 px-2 text-sm font-medium text-gray-600 bg-white rounded-lg dark:bg-gray-900 dark:text-gray-400">
         {{ $title }}
