d8c0b309e18345768cf012f6d53db204210e040c
199ocero <199ocero@gmail.com>
2024-09-07 19:46:56 +0800

    added attribute to card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,4 @@
-<div class="p-5 bg-gray-50 rounded-lg border border-gray-300 dark:border-gray-700 dark:bg-gray-800">
+<div
+    {{ $attributes->merge(['class' => 'p-5 bg-gray-50 rounded-lg border border-gray-300 dark:border-gray-700 dark:bg-gray-800']) }}>
     {{ $slot }}
 </div>
