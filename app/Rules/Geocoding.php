b7e8bc2811366f1282168de76af6a78457d3c606
199ocero <199ocero@gmail.com>
2024-09-07 17:46:49 +0800

    added rule for geocoding format

---
+++ app/Rules/Geocoding.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class Geocoding implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!is_string($value) || !preg_match('/^[^,]+,\s*[A-Z]{2}$/', $value)) {
+            $fail('The :attribute must be in the format "City, CountryCode" (e.g., "London, GB").');
+        }
+    }
+}
